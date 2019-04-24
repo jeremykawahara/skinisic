ce7a1eb37ba681a6036a7e1477e8e8c2e4fde4ed
Jeremy Kawahara <jeremy.kawahara@gmail.com>
2019-04-23 18:08:31 -0700

    More options in image plotting

--- skinisic/vis.py
+++ skinisic/vis.py
@@ -1,20 +1,22 @@
 import matplotlib.pyplot as plt
 
 
-def plot_img_preds(img, y_pred, labels):
+def plot_img_preds(img, y_pred, labels, figsize=(16,4), fontsize=12, show_colorbar=True):
     n_rows = 1
     n_cols = len(labels) + 1
 
-    plt.figure(figsize=(16, 4))
+    plt.figure(figsize=figsize)
     plt.subplot(n_rows, n_cols, 1)
     plt.imshow(img)
-    plt.title('Image')
+    plt.title('Image', fontsize=fontsize)
     plt.axis('off')
-    plt.colorbar(fraction=0.04, pad=0.04)
+    if show_colorbar:
+        plt.colorbar(fraction=0.04, pad=0.04)
 
     for c, label in enumerate(labels):
         plt.subplot(n_rows, n_cols, c + 2)
         plt.imshow(y_pred[:, :, c])
-        plt.title(label)
-        plt.colorbar(fraction=0.04, pad=0.04)
+        plt.title(label, fontsize=fontsize)
         plt.axis('off')
+        if show_colorbar:
+            plt.colorbar(fraction=0.04, pad=0.04)
