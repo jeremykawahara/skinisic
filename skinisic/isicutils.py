c97b6c3e6bdfdc4accd6239dd5f278bc3ed3125f
jeremykawahara <jeremy.kawahara@gmail.com>
2019-06-21 20:27:03 -0700

    Convert the superpixels to segmentation

--- skinisic/isicutils.py
+++ skinisic/isicutils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skinisic.converter import compute_superseg
 
 
 def rgb2index(rgb_pixel):
@@ -11,3 +12,18 @@ def rgb2index(rgb_pixel):
         (rgb_pixel[..., 0].astype(np.uint64)) + \
         (rgb_pixel[..., 1].astype(np.uint64) << np.uint64(8)) + \
         (rgb_pixel[..., 2].astype(np.uint64) << np.uint64(16))
+
+
+def superpixels2superseg(superpixels, json_dict, dermoscopic_labels):
+    # Convert so each superpixel has a unique index.
+    superindexes = rgb2index(superpixels)
+
+    # Convert the dictonary to an array.
+    json_array = np.asarray([json_dict[label] for label in dermoscopic_labels], dtype=np.float32)
+    # print(json_array.shape)
+
+    # Convert the superpixel labels to a superpixel segmentation,
+    # where each pixel in each of the 4 dermoscopic features are labeled.
+    superseg = compute_superseg(superindexes, json_array)
+    # print(superseg.shape)
+    return superseg
